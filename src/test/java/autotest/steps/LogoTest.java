713f093c817760cdd8d1911946daf84ac073d546
Andrey Roofin <andrey.roofin@gmail.com>
2026-06-29 18:20:42 +0700

    logo tests

--- src/test/java/autotest/steps/LogoTest.java
+++ src/test/java/autotest/steps/LogoTest.java
@@ -19,4 +19,30 @@ public class LogoTest {
         this.pageObject = pageObject;
         this.playwrightConfig = playwrightConfig;
     }
+
+    @Given("I open the Playwright logo page")
+    public void iOpenThePlaywrightTestPage() {
+        pageObject
+                .getPage()
+                .navigate(playwrightConfig.getTestPageUrl());
+    }
+
+    @When("I check for the logo")
+    public void iCheckForTheLogo() {
+        boolean logoExists = pageObject
+                    .getPage().locator(Locators.LOGO_SELECTOR)
+                    .count() > 0;
+
+        assertTrue("Логотип должен присутствовать на странице", logoExists);
+    }
+
+    @Then("I verify the logo is displayed")
+    public void iVerifyTheLogoIsDisplayed() {
+        boolean logoVisible = pageObject
+                    .getPage()
+                    .locator(Locators.LOGO_SELECTOR)
+                    .isVisible();
+
+        assertTrue("Логотип должен быть виден", logoVisible);
+    }
 }
